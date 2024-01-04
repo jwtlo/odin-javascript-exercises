7a9228253ab1fc21673f1837ea944b4b295a68d2
Jack Lo <waitunglo.02@gmail.com>
2024-01-04 15:36:48 -0800

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const strArray = str.split("");
+  strArray.reverse();
+  return strArray.join("");
 };
 
 // Do not edit below this line
