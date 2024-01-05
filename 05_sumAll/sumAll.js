9a8d40f13a1fca063847fe6d34daf9f55f0f8703
Jack Lo <waitunglo.02@gmail.com>
2024-01-04 16:33:06 -0800

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+  if (first < 0 || second < 0) return "ERROR";
+  if (typeof first != 'number' || typeof second != 'number') return "ERROR";
 
+
+  let smaller, larger;
+  if (first > second) {
+    smaller = second;
+    larger = first;
+  } else {
+    larger = second;
+    smaller = first;
+  }
+  return (smaller + larger) * (larger - smaller + 1) / 2
 };
 
 // Do not edit below this line
