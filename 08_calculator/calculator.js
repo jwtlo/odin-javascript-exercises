79992e510ce038c60f763be7fed72d7c7466f605
Jack Lo <waitunglo.02@gmail.com>
2024-01-05 15:30:43 -0800

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, item) => acc * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+
+  let acc = 1;
+  for (let i = a; i > 1; i--) {
+    acc *= i;
+  }
+  return acc;
 };
 
 // Do not edit below this line
