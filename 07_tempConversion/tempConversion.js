8a8232863d49a317e620e74fabbc60e90694cb3c
Jack Lo <waitunglo.02@gmail.com>
2024-01-04 17:48:33 -0800

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return +((f - 32) * 5 / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return +(c * 9 / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
