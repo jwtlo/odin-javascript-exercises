ebb7ac9a1fe1f7622374e65ad5d3ad35592b5f89
Jack Lo <waitunglo.02@gmail.com>
2024-01-05 16:14:56 -0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  if (+x < 0 ) return "OOPS";
+  if (+x === 0) return 0;
 
+  let prev = curr = 1;
+  for (let i = 2; i < +x; i ++) {
+    [prev, curr] = [curr, curr + prev];
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
