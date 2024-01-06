95f98de2cf6cffdbf05f6752144a68c7a25cdb06
Jack Lo <waitunglo.02@gmail.com>
2024-01-05 16:09:28 -0800

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let clean = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  console.log(clean);
+  len = clean.length;
+  for (let i = 0; i < len/2; i++) {
+    console.log(clean[i], clean.at(-i-1));
+    if (clean[i] !== clean.at(-i-1)) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
