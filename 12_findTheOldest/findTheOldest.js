5a3d5145fa292177834457c536ca1472b6ad8ce8
Jack Lo <waitunglo.02@gmail.com>
2024-01-05 16:32:07 -0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let sorted = arr.sort((person1, person2) => {
+    let person1Age = (("yearOfDeath" in person1) ? person1.yearOfDeath : 2024) - person1.yearOfBirth;
+    let person2Age = (("yearOfDeath" in person2) ? person2.yearOfDeath : 2024) - person2.yearOfBirth;
+    return person1Age > person2Age ? -1 : 1;
+  });
+  return sorted[0]
 };
 
 // Do not edit below this line
